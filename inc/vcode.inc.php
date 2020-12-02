6104c7f8ea93dbd4ad6e60238944764c1ee706c9
ChopperCP <568624486@qq.com>
2020-12-02 18:00:59 +0800

    Fix the bug that the CAPCHA won't show

--- inc/vcode.inc.php
+++ inc/vcode.inc.php
@@ -1,6 +1,10 @@
 <?php 
 //实现验证码功能
 function vcode($width=120,$height=40,$fontSize=30,$countElement=5,$countPixel=100,$countLine=4){
+	//找到字体的绝对路径
+	$fontpath=dirname(__FILE__).'\..\font\ManyGifts.ttf';
+	$fontpath=realpath($fontpath);
+
 	header('Content-type:image/jpeg');
 	$element=array('a','b','c','d','e','f','g','h','i','j','k','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
 	$string='';
@@ -19,9 +23,24 @@ function vcode($width=120,$height=40,$fontSize=30,$countElement=5,$countPixel=10
 		imageline($img,rand(0,$width/2),rand(0,$height),rand($width/2,$width),rand(0,$height),imagecolorallocate($img,rand(100,200),rand(100,200),rand(100,200)));
 	}
 	//imagestring($img,5,0,0,'abcd',$colorString);
-	imagettftext($img,$fontSize,rand(-5,5),rand(5,15),rand(30,35),$colorString,'font/ManyGifts.ttf',$string);
+	imagettftext($img,$fontSize,rand(-5,5),rand(5,15),rand(30,35),$colorString,
+	$fontpath,$string);	//使用相对路径打不开
 	imagejpeg($img);
 	imagedestroy($img);
 	return $string;
 }
+
+// echo getcwd();
+// echo "<br />";
+
+// //获取某目录下所有文件、目录名（不包括子目录下文件、目录名）
+// if ($handle = opendir('./font')) {
+//     while (false !== ($entry = readdir($handle))) {
+//         if ($entry != "." && $entry != "..") {
+//             echo "$entry\n";
+//         }
+//     }
+//     closedir($handle);
+// }
+vcode(100,40,30,4);
 ?>
